9c0f9d7b0712762aa18aba3404cd98836b9d3d89
Mehdi Lamriben <mehdilmbn@gmail.com>
2021-04-13 12:31:38 +0200

    update README

--- rails.rb
+++ rails.rb
@@ -12,7 +12,7 @@ inject_into_file 'Gemfile', before: 'group :development, :test do' do
   RUBY
 end
 
-inject_into_file 'Gemfile', after: 'group :development, :test do' do
+inject_into_file 'Gemfile', after: 'group :development, :test do ' do
   <<-RUBY
   gem 'rspec'
 
